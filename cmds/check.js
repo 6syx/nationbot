e034702c294aba22e97a2f88f662c97d9ad8cbae
leo <somebody1337666@gmail.com>
2022-07-03 15:32:53 -0500

    s

--- cmds/check.js
+++ cmds/check.js
@@ -22,7 +22,7 @@ module.exports = {
         if (!uid) return interaction.reply(`This user does not exist on Roblox.`, { ephemeral: true })
         if (!config.management.administrators.find(s => s == usera) && !config.management.lowerusers.find(s => s == usera)) return interaction.reply({ content: "You are not whitelisted to use this bot's administrative functions. Contact its owner if you feel this is a mistake.", ephemeral: true})
         blacklistedgroups1 = 0
-		const userGroups = await roblox.getGroups(immigrants[i].userId)
+		const userGroups = await roblox.getGroups(uid)
 		for (f = 0; f < userGroups.length; f++) {
 		  for (l = 0; l < blacklistedgroups.length; l++) {
 			if (blacklistedgroups[l] == userGroups[f].Id) {
@@ -32,8 +32,8 @@ module.exports = {
 		  }
 		}
         for (f = 0; f < blacklistedusers.length; f++) {
-            if (blacklistedusers[f] == uID) {
-                await roblox.setRank(config.groupid, uID, Number(config.immigration.failrank))
+            if (blacklistedusers[f] == uid) {
+                await roblox.setRank(config.groupid, uid, Number(config.immigration.failrank))
                 let iEmbed = new discord.MessageEmbed()
                     .setTitle('Fail')
                     .setColor('RED')
@@ -45,14 +45,14 @@ module.exports = {
         }
         if (config.immigration.settings.distinguishment.enabled == true) {
 			for (f = 0; f < config.immigration.settings.distinguishment.list.length; f++) {
-				if (config.immigration.settings.distinguishment.list[f] == immigrants[i].userId) {
+				if (config.immigration.settings.distinguishment.list[f] == uid) {
 				return interaction.reply({ content: 'This user is a distinguished individual and deemed eligible for immigration.', ephemeral: true })
 				}
 			}
 		}
         if (config.immigration.settings.majororganization.enabled == true) {
             if (await roblox.getRankInGroup(config.immigration.settings.majororganization.groupid, uID) >= config.immigration.settings.majororganization.org_rank_id) {
-            await roblox.setRank(config.groupid, uID, Number(config.immigration.settings.majororganization.your_foreign_rank_id))
+            await roblox.setRank(config.groupid, uid, Number(config.immigration.settings.majororganization.your_foreign_rank_id))
             let iEmbed = new discord.MessageEmbed()
                 .setTitle('Success')
                 .setColor('GREEN')
@@ -62,9 +62,9 @@ module.exports = {
             return interaction.reply({ content: 'This user is a foreign representative and has been ranked accordingly.', ephemeral: true })
             }
         }
-        const player = await roblox.getPlayerInfo(uID)
+        const player = await roblox.getPlayerInfo(uid)
         if (player.age <= config.immigration.settings.agelimit) {
-            await roblox.setRank(config.groupid, uID, config.immigration.failedrank)
+            await roblox.setRank(config.groupid, uid, config.immigration.failedrank)
             let iEmbed = new discord.MessageEmbed()
                 .setTitle('Fail')
                 .setColor('RED')
@@ -74,7 +74,7 @@ module.exports = {
             interaction.reply({ content: 'This user has been caught as underage and has been deemed ineligible for immigration.', ephemeral: true })
         }
         if (failedcheck == true) {
-            await roblox.setRank(config.groupid, uID, Number(config.immigration.failedrank))
+            await roblox.setRank(config.groupid, uid, Number(config.immigration.failedrank))
             let iEmbed = new discord.MessageEmbed()
                 .setTitle('Fail')
                 .setColor('RED')
